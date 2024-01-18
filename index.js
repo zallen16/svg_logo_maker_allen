f82025feeab655fd3385cd6b7b283b9266ab13bd
zallen16 <126983111+zallen16@users.noreply.github.com>
2024-01-18 00:33:03 -0500

    added questions variable

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+
+class Svg{
+    constructor(){
+        this.textInput = textInput;
+        this.shapeInput = shapeInput;
+    }
+    render(){
+
+    }
+};
+
+const questions = [
+    {
+        type: "input",
+        name: "text",
+        message: "Please enter up to three characters.",
+    },
+    {
+        type: "input",
+        name: "text-color",
+        message: "Please enter a color keyword (OR a hexadecimal number) for the text.",
+    },
+    {
+        type: "input",
+        name: "image",
+        message: "Which shape would you like?",
+        choices: ["Cirlce", "Square", "Triangle"],
+    },
+    {
+        type: "input",
+        name: "shape-color",
+        message: 
+    }
+];
