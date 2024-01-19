d1aacb330026304b3796e09288175c44f1b7d847
zallen16 <126983111+zallen16@users.noreply.github.com>
2024-01-18 23:38:11 -0500

    finished each shape class code, created generatelogo

---
+++ generateLogo.js
@@ -0,0 +1,7 @@
+const Circle = require("../lib/circle.js")
+const Rectangle = require("../lib/rectangle.js")
+const Triangle = require("../lib/triangle.js")
+
+function generateLogo(data) {
+    let shape = 
+}
